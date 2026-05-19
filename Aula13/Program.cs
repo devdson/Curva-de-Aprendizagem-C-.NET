88ae047a6f4e3f0316fb7c3c6c2d01d951300add
Davidson Macedo <103448810+devdson@users.noreply.github.com>
2026-05-19 16:11:39 -0300

    Add files via upload

---
+++ Aula13/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Aula13;
+
+public class Program{
+
+    public static void Main(){
+
+        // Declaração de Array
+        string[] frutas = {"Maçã", "Banana", "Morango"};
+
+        // Acessando Índice
+        Console.WriteLine(frutas[1]);
+
+    }
+}
---
+++ Aula14/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Aula14;
+
+public class Program{
+
+    public static void Main(){
+
+        // Declaração com inicialização de valores (Lista)
+        List<string> frutas = new List<string>()
+        {
+            "Uva",
+            "Maçã",
+            "Banana"
+        };
+
+        // Acessar os valores
+        Console.WriteLine(fruits[2]);
+    }
+}
