8a8d164449fe9c907c83f5d36e19de14a4d0c6fb
Davidson Macedo <103448810+devdson@users.noreply.github.com>
2026-05-19 16:07:42 -0300

    Add files via upload

---
+++ Aula02/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Aula02;
+
+public class Program
+{
+
+    public static void Main()
+    {
+
+    // Variáveis
+
+        string nome = "Davidson";
+        Console.WriteLine("Nome: " + nome);
+
+        int numero = 20;
+        Console.WriteLine("Número inteiro: " + numero);
+
+        double numeroQuebrado = 5.2;
+        Console.WriteLine("Número decimal: " + numeroQuebrado);
+
+        char letra = 'a';
+        Console.WriteLine("Letra: " +letra);
+
+        bool boolean = true;
+
+    }
+}
